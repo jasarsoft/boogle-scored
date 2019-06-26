3a4751d63106e65dffcc227ca22fd0425b8feb62
Edin Jašarević <edinjasar14@gmail.com>
2019-06-26 19:50:59 +0200

    prva funkcija

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,5 +8,38 @@ namespace ConsoleApp3
         {
             Console.WriteLine("Hello World!");
         }
+
+        static int Scored(string word)
+        {
+            int point = 0;
+
+            switch (word.Length)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    point = 0;
+                    break;
+                case 3:
+                case 4:
+                    point += 1;
+                    break;
+                case 5:
+                    point += 2;
+                    break;
+                case 6:
+                    point += 3;
+                    break;
+                case 7:
+                    point += 5;
+                    break;
+                
+                default:
+                    point += 11;
+                    break;
+            }
+
+            return point;
+        }
     }
 }
